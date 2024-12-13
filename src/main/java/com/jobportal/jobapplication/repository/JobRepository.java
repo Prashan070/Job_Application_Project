4bb6f6d5417d8aba3ca07e540329bf6d7a5a87aa
Prashan070 <prashansrivastava77@gmail.com>
2024-12-14 02:29:33 +0530

    Project Structure update

--- src/main/java/com/jobportal/jobapplication/repository/JobRepository.java
+++ src/main/java/com/jobportal/jobapplication/repository/JobRepository.java
@@ -2,6 +2,8 @@ package com.jobportal.jobapplication.repository;
 
 import com.jobportal.jobapplication.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobRepository extends JpaRepository<Job, Long>{
 }
--- src/main/java/com/jobportal/jobapplication/service/JobServiceImpl.java
+++ src/main/java/com/jobportal/jobapplication/service/JobServiceImpl.java
@@ -1,5 +1,8 @@
 package com.jobportal.jobapplication.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JobServiceImpl implements JobService{
 
 
